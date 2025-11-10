a544c4ca3a094fe46c1b32e84ca796ed6271bb15
anastasiia-konstantynova <ana.konstantynova@gmail.com>
2025-11-10 20:57:45 +0100

    feat: added trending movie page
    Added deserialisation of API response.
    Added a template for `trending` movies and added `css` for it.

--- src/main/java/com/swdev/springbootproject/controller/TrendsController.java
+++ src/main/java/com/swdev/springbootproject/controller/TrendsController.java
@@ -1,14 +1,15 @@
 package com.swdev.springbootproject.controller;
 
+import com.swdev.springbootproject.model.Movie;
 import com.swdev.springbootproject.service.TMDBService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.Map;
 
-@RestController
+@Controller
 public class TrendsController {
 
     private final TMDBService tmdbService;
@@ -18,7 +19,14 @@ public class TrendsController {
     }
 
     @GetMapping("/api/trends")
-    public String getTrends() {
+    @ResponseBody
+    public List<Movie> getTrends() {
         return tmdbService.getPopularMovies();
+    }
+
+    @GetMapping("/trends")
+    public String showTrendingMovies(Model model) {
+        model.addAttribute("movies", tmdbService.getPopularMovies());
+        return "trends";
     }
 }
--- src/main/java/com/swdev/springbootproject/service/TMDBService.java
+++ src/main/java/com/swdev/springbootproject/service/TMDBService.java
@@ -1,10 +1,14 @@
 package com.swdev.springbootproject.service;
 
+import com.swdev.springbootproject.model.Movie;
+import com.swdev.springbootproject.model.TMDBApiResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
+import java.util.Objects;
 
 
 @Service
@@ -18,7 +22,11 @@ public class TMDBService {
         this.restClient = restClientBuilder.baseUrl("https://api.themoviedb.org/3").build();
     }
 
-    public String getPopularMovies() {
-        return this.restClient.get().uri("/movie/popular?api_key=" + apiKey).retrieve().body(String.class);
+    public List<Movie> getPopularMovies() {
+        return Objects.requireNonNull(this.restClient
+                .get()
+                .uri("/movie/popular?api_key=" + apiKey)
+                .retrieve()
+                .body(TMDBApiResponse.class)).getResults();
     }
 }
