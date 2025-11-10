586dbc79a231ba86be9c386978658350988860d6
anastasiia-konstantynova <ana.konstantynova@gmail.com>
2025-11-10 20:49:39 +0100

    Create Movie.java
    Model for a `Movie` object

---
+++ src/main/java/com/swdev/springbootproject/model/Movie.java
@@ -0,0 +1,28 @@
+package com.swdev.springbootproject.model;
+
+public class Movie {
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPosterPath(String posterUrl) {
+        this.posterPath = posterUrl;
+    }
+
+    private String title;
+    private String posterPath;
+
+    public String getPosterUrl() {
+        String posterBaseUrl = "https://image.tmdb.org/t/p/w500";
+        return posterBaseUrl + posterPath;
+    }
+}
+
