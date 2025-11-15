2609fdd653cd1404dd133e4956a532e24805433f
Noah Jutz <noah@noahjutz.com>
2025-11-15 15:24:28 +0100

    Auto-Format everything

--- src/main/java/com/swdev/springbootproject/repository/UserRepository.java
+++ src/main/java/com/swdev/springbootproject/repository/UserRepository.java
@@ -1,15 +1,14 @@
 package com.swdev.springbootproject.repository;
 
 import com.swdev.springbootproject.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+  Optional<User> findByEmail(String email);
 
-    boolean existsByEmail(String email);
+  boolean existsByEmail(String email);
 }
--- src/main/java/com/swdev/springbootproject/service/TMDBService.java
+++ src/main/java/com/swdev/springbootproject/service/TMDBService.java
@@ -2,31 +2,30 @@ package com.swdev.springbootproject.service;
 
 import com.swdev.springbootproject.model.Movie;
 import com.swdev.springbootproject.model.TMDBApiResponse;
+import java.util.List;
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.util.List;
-import java.util.Objects;
-
-
 @Service
 public class TMDBService {
-    private final RestClient restClient;
+  private final RestClient restClient;
 
-    @Value("${tmdb.api.key:keynotfound}")
-    private String apiKey;
+  @Value("${tmdb.api.key:keynotfound}")
+  private String apiKey;
 
-    public TMDBService(RestClient.Builder restClientBuilder) {
-        this.restClient = restClientBuilder.baseUrl("https://api.themoviedb.org/3").build();
-    }
+  public TMDBService(RestClient.Builder restClientBuilder) {
+    this.restClient = restClientBuilder.baseUrl("https://api.themoviedb.org/3").build();
+  }
 
-    public List<Movie> getPopularMovies() {
-        return Objects.requireNonNull(this.restClient
+  public List<Movie> getPopularMovies() {
+    return Objects.requireNonNull(
+            this.restClient
                 .get()
                 .uri("/movie/popular?api_key=" + apiKey)
                 .retrieve()
-                .body(TMDBApiResponse.class)).getResults();
-    }
+                .body(TMDBApiResponse.class))
+        .getResults();
+  }
 }
