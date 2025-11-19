47e5952623b3d37e5f4c7cfab312f605f0b7b595
QuocViet-Pham <72968949+QuocViet-Pham@users.noreply.github.com>
2025-11-19 13:59:33 +0100

    Errorpage 004
    Fix formatting, apply maven spotless

--- src/main/java/com/swdev/springbootproject/controller/CustomErrorController.java
+++ src/main/java/com/swdev/springbootproject/controller/CustomErrorController.java
@@ -1,29 +1,27 @@
 package com.swdev.springbootproject.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request, Model model) {
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        String errorMessage = (String) request.getAttribute("javax.servlet.error.message");
+  @RequestMapping("/error")
+  public String handleError(HttpServletRequest request, Model model) {
+    Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+    String errorMessage = (String) request.getAttribute("javax.servlet.error.message");
 
-        model.addAttribute("statusCode", statusCode);
-        model.addAttribute("errorMessage", errorMessage);
+    model.addAttribute("statusCode", statusCode);
+    model.addAttribute("errorMessage", errorMessage);
 
-        return "customError"; // return custom error view
-    }
+    return "customError"; // return custom error view
+  }
 
-    @Override
-    public String getErrorPath() {
-        return "/error";
-    }
+  @Override
+  public String getErrorPath() {
+    return "/error";
+  }
 }
-
--- src/main/java/com/swdev/springbootproject/model/CustomizedErrorAttributes.java
+++ src/main/java/com/swdev/springbootproject/model/CustomizedErrorAttributes.java
@@ -1,30 +1,29 @@
 package com.swdev.springbootproject.model;
 
+import java.util.Map;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.WebRequest;
 
-import java.util.Map;
-
 @Component
 public class CustomizedErrorAttributes extends DefaultErrorAttributes {
 
-    @Override
-    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
-        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+  @Override
+  public Map<String, Object> getErrorAttributes(
+      WebRequest webRequest, ErrorAttributeOptions options) {
+    Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
 
-        // Customize the error attributes
-        errorAttributes.put("locale", webRequest.getLocale().toString());
-        errorAttributes.put("customMessage", "This is a custom error message");
+    // Customize the error attributes
+    errorAttributes.put("locale", webRequest.getLocale().toString());
+    errorAttributes.put("customMessage", "This is a custom error message");
 
-        Throwable error = getError(webRequest);
-        if (error != null) {
-            errorAttributes.put("exception", error.getClass().getName());
-            errorAttributes.put("message", error.getMessage());
-        }
-
-        return errorAttributes;
+    Throwable error = getError(webRequest);
+    if (error != null) {
+      errorAttributes.put("exception", error.getClass().getName());
+      errorAttributes.put("message", error.getMessage());
     }
-}
 
+    return errorAttributes;
+  }
+}
