b2983438da6844e912c7801e2b4834d98fa192a8
QuocViet-Pham <72968949+QuocViet-Pham@users.noreply.github.com>
2025-11-19 14:10:26 +0100

    Errorpage 006
    Fix formatting problem (maven spotless)

--- src/main/java/com/swdev/springbootproject/controller/GlobalExceptionHandler.java
+++ src/main/java/com/swdev/springbootproject/controller/GlobalExceptionHandler.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public String handleException(Exception ex, Model model) {
-        model.addAttribute("errorMessage", ex.getMessage());
-        return "error";
-    }
+  @ExceptionHandler(Exception.class)
+  public String handleException(Exception ex, Model model) {
+    model.addAttribute("errorMessage", ex.getMessage());
+    return "error";
+  }
 }
-
