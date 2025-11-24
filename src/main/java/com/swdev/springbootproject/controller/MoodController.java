dc27530edf1085d2f25ff0c15621cfab2515d09d
anastasiia-konstantynova <ana.konstantynova@gmail.com>
2025-11-24 10:57:09 +0100

    Implement mood-based movie filtering
    - Create `/mood` endpoint with default mood.
    - Retrieve movies from TMDB API filtered by mood-based genre combinations.
    - Create mood selection page with dropdown.

--- src/main/java/com/swdev/springbootproject/controller/MoodController.java
+++ src/main/java/com/swdev/springbootproject/controller/MoodController.java
@@ -2,13 +2,15 @@ package com.swdev.springbootproject.controller;
 
 import com.swdev.springbootproject.model.Movie;
 import com.swdev.springbootproject.service.TMDBService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
-@RestController
+@Controller
 public class MoodController {
     private final TMDBService tmdbService;
 
@@ -19,5 +21,16 @@ public class MoodController {
     @GetMapping("/api/mood")
     public List<Movie> getMood(@RequestParam(required = false) String mood) {
         return tmdbService.getMoviesByMood(mood);
+    }
+
+    @GetMapping("/mood")
+    public String showMoviesByMood(@RequestParam(name = "mood", required = false) String mood, Model model) {
+
+        if (mood == null || mood.isBlank()) {
+            mood = "happy";
+        }
+        model.addAttribute("mood", mood);
+        model.addAttribute("movies", tmdbService.getMoviesByMood(mood));
+        return "mood";
     }
 }
--- src/main/java/com/swdev/springbootproject/service/TMDBService.java
+++ src/main/java/com/swdev/springbootproject/service/TMDBService.java
@@ -16,53 +16,54 @@ import org.springframework.web.client.RestClient;
 
 @Service
 public class TMDBService {
-  private final RestClient restClient =
-      RestClient.builder().baseUrl("https://api.themoviedb.org/3").build();
+    private final RestClient restClient =
+            RestClient.builder().baseUrl("https://api.themoviedb.org/3").build();
 
-  @Value("${tmdb.api.key:keynotfound}")
-  private String apiKey;
+    @Value("${tmdb.api.key:keynotfound}")
+    private String apiKey;
 
-  public List<Movie> getPopularMovies() {
-    return Objects.requireNonNull(
-            this.restClient
+    public List<Movie> getPopularMovies() {
+        return Objects.requireNonNull(
+                        this.restClient
+                                .get()
+                                .uri("/movie/popular?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&certification_country=US&certification.lte=PG&api_key=" + apiKey)
+                                .retrieve()
+                                .body(TMDBApiResponse.class))
+                .getResults();
+    }
+
+    public List<Movie> getMoviesByMood(String mood) {
+        Map<String, List<Genre>> moodToGenre = new HashMap<>(Map.ofEntries(
+                Map.entry("adventurous", List.of(Genre.ACTION, Genre.FANTASY)),
+                Map.entry("happy", List.of(Genre.COMEDY, Genre.FAMILY)),
+                Map.entry("brave", List.of(Genre.HORROR)),
+                Map.entry("nostalgic", List.of(Genre.HISTORY)),
+                Map.entry("romantic", List.of(Genre.COMEDY, Genre.ROMANCE)),
+                Map.entry("sad", List.of(Genre.DRAMA)),
+                Map.entry("contemplative", List.of(Genre.SCIFI, Genre.ACTION)),
+                Map.entry("curious", List.of(Genre.DOCUMENTARY)),
+                Map.entry("edgy", List.of(Genre.CRIME)),
+                Map.entry("silly", List.of(Genre.ANIMATION, Genre.COMEDY)),
+                Map.entry("cozy", List.of(Genre.FAMILY, Genre.ANIMATION)),
+                Map.entry("detective-like", List.of(Genre.MYSTERY)),
+                Map.entry("melodic", List.of(Genre.MUSIC))
+        ));
+
+        List<Genre> genres = moodToGenre.getOrDefault(mood, List.of(Genre.ADVENTURE));
+
+        String genreParam = genres.stream()
+                .map(Genre::getGenreId)
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+
+
+        TMDBApiResponse tmdbApiResponse = this.restClient
                 .get()
-                .uri("/movie/popular?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&api_key=" + apiKey)
+                .uri("/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&certification_country=US&certification.lte=PG-13&with_genres=" + genreParam + "&api_key=" + apiKey)
                 .retrieve()
-                .body(TMDBApiResponse.class))
-        .getResults();
-  }
+                .body(TMDBApiResponse.class);
 
-  public List<Movie> getMoviesByMood(String mood) {
-      Map<String, List<Genre>> moodToGenre = new HashMap<>(Map.ofEntries(
-              Map.entry("adventurous", List.of(Genre.ADVENTURE, Genre.MYSTERY, Genre.ACTION, Genre.WESTERN)),
-              Map.entry("happy", List.of(Genre.COMEDY, Genre.ANIMATION, Genre.ANIMATION, Genre.FAMILY)),
-              Map.entry("brave", List.of(Genre.HORROR, Genre.THRILLER, Genre.CRIME)),
-              Map.entry("nostalgic", List.of(Genre.HISTORY)),
-              Map.entry("romantic", List.of(Genre.ROMANCE)),
-              Map.entry("sad", List.of(Genre.DRAMA)),
-              Map.entry("contemplative", List.of(Genre.SCIFI)),
-              Map.entry("curious", List.of(Genre.DOCUMENTARY)),
-              Map.entry("edgy", List.of(Genre.HORROR, Genre.CRIME)),
-              Map.entry("silly", List.of(Genre.COMEDY)),
-              Map.entry("cozy", List.of(Genre.FAMILY, Genre.ROMANCE, Genre.ANIMATION)),
-              Map.entry("detective-like", List.of(Genre.MYSTERY, Genre.THRILLER))
-      ));
-
-      List<Genre> genres = moodToGenre.getOrDefault(mood, List.of(Genre.ADVENTURE));
-
-      String genreParam = genres.stream()
-              .map(Genre::getGenreId)
-              .map(String::valueOf)
-              .collect(Collectors.joining(","));
-
-
-      TMDBApiResponse tmdbApiResponse = this.restClient
-              .get()
-              .uri("/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc&with_genres=" + genreParam + "&api_key=" + apiKey)
-              .retrieve()
-              .body(TMDBApiResponse.class);
-
-      assert tmdbApiResponse != null;
-      return tmdbApiResponse.getResults();
-  }
+        assert tmdbApiResponse != null;
+        return tmdbApiResponse.getResults();
+    }
 }
