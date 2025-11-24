df7a05abd64b644031a92ccd8be3f3481acf1bcf
anastasiia-konstantynova <ana.konstantynova@gmail.com>
2025-11-24 21:51:13 +0100

    Run formatter

--- src/main/java/com/swdev/springbootproject/controller/MoodController.java
+++ src/main/java/com/swdev/springbootproject/controller/MoodController.java
@@ -2,35 +2,34 @@ package com.swdev.springbootproject.controller;
 
 import com.swdev.springbootproject.model.Movie;
 import com.swdev.springbootproject.service.TMDBService;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-import java.util.List;
-
 @Controller
 public class MoodController {
-    private final TMDBService tmdbService;
+  private final TMDBService tmdbService;
 
-    public MoodController(TMDBService tmdbService) {
-        this.tmdbService = tmdbService;
+  public MoodController(TMDBService tmdbService) {
+    this.tmdbService = tmdbService;
+  }
+
+  @GetMapping("/api/mood")
+  public List<Movie> getMood(@RequestParam(required = false) String mood) {
+    return tmdbService.getMoviesByMood(mood);
+  }
+
+  @GetMapping("/mood")
+  public String showMoviesByMood(
+      @RequestParam(name = "mood", required = false) String mood, Model model) {
+
+    if (mood == null || mood.isBlank()) {
+      mood = "happy";
     }
-
-    @GetMapping("/api/mood")
-    public List<Movie> getMood(@RequestParam(required = false) String mood) {
-        return tmdbService.getMoviesByMood(mood);
-    }
-
-    @GetMapping("/mood")
-    public String showMoviesByMood(@RequestParam(name = "mood", required = false) String mood, Model model) {
-
-        if (mood == null || mood.isBlank()) {
-            mood = "happy";
-        }
-        model.addAttribute("mood", mood);
-        model.addAttribute("movies", tmdbService.getMoviesByMood(mood));
-        return "mood";
-    }
+    model.addAttribute("mood", mood);
+    model.addAttribute("movies", tmdbService.getMoviesByMood(mood));
+    return "mood";
+  }
 }
