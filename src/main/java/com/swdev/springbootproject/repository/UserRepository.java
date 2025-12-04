cf82b00e7ea137ced2dc71f2507033c4a6af12d4
Noah Jutz <noah@noahjutz.com>
2025-12-04 13:14:34 +0100

    Fix IDE warnings in UserRepository
    annotate generics with NonNull and remove unused findByEmail()

--- src/main/java/com/swdev/springbootproject/repository/UserRepository.java
+++ src/main/java/com/swdev/springbootproject/repository/UserRepository.java
@@ -2,13 +2,12 @@ package com.swdev.springbootproject.repository;
 
 import com.swdev.springbootproject.entity.User;
 import java.util.Optional;
+
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-  Optional<User> findByEmail(String email);
-
+public interface UserRepository extends JpaRepository<@NonNull User, @NonNull Long> {
   boolean existsByEmail(String email);
 }
