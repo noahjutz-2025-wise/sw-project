3c2babb405e6fb2c0004788e75bdbd4a62dbed4e
Noah Jutz <noah@noahjutz.com>
2025-12-04 12:36:45 +0100

    Use lombok in User Entity

--- src/main/java/com/swdev/springbootproject/entity/User.java
+++ src/main/java/com/swdev/springbootproject/entity/User.java
@@ -1,7 +1,11 @@
 package com.swdev.springbootproject.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "users")
 public class User {
@@ -28,46 +32,6 @@ public class User {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
-    this.password = password;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
     this.password = password;
   }
 
