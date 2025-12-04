3a32fb64926390b7df07e53ded0cb1081774e8ec
Noah Jutz <noah@noahjutz.com>
2025-12-04 17:10:54 +0100

    Change User Entity schema
    - Combined firstName and lastName into name
    - kept long id, that way users can change their email later

--- src/main/java/com/swdev/springbootproject/entity/User.java
+++ src/main/java/com/swdev/springbootproject/entity/User.java
@@ -18,10 +18,7 @@ public class User {
   private Long id;
 
   @Column(nullable = false)
-  private String firstName;
-
-  @Column(nullable = false)
-  private String lastName;
+  private String name;
 
   @Column(nullable = false, unique = true)
   private String email;
