699fb486eca862caa7a124456a5229a7fbc62b4f
Noah Jutz <noah@noahjutz.com>
2025-12-04 12:55:20 +0100

    Fix import format

--- src/main/java/com/swdev/springbootproject/controller/SignupController.java
+++ src/main/java/com/swdev/springbootproject/controller/SignupController.java
@@ -2,7 +2,6 @@ package com.swdev.springbootproject.controller;
 
 import com.swdev.springbootproject.entity.User;
 import com.swdev.springbootproject.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/swdev/springbootproject/model/TMDBApiResponse.java
+++ src/main/java/com/swdev/springbootproject/model/TMDBApiResponse.java
@@ -1,8 +1,7 @@
 package com.swdev.springbootproject.model;
 
-import lombok.Getter;
-
 import java.util.List;
+import lombok.Getter;
 
 @Getter
 public class TMDBApiResponse {
