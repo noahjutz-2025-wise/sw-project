b901d04046a1293cb83e1571b676a09a1a56d3e8
Noah Jutz <noah@noahjutz.com>
2025-12-10 17:34:05 +0100

    Make UserDetails use SQLite db for credentials
    Currently broken because the current User schema uses a `name` column
    instead of Spring Boot's default `username`.

--- src/main/java/com/swdev/springbootproject/config/SecurityConfig.java
+++ src/main/java/com/swdev/springbootproject/config/SecurityConfig.java
@@ -4,10 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 class SecurityConfig {
+  private final PasswordEncoder enc = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http.authorizeHttpRequests(
@@ -20,5 +29,14 @@ class SecurityConfig {
                     .authenticated())
         .formLogin(Customizer.withDefaults())
         .build();
+  }
+
+  @Bean
+  UserDetailsService auth(DataSource ds) {
+    final var manager = new JdbcUserDetailsManager(ds);
+    final var admin =
+        User.builder().username("admin").password(enc.encode("admin")).roles("ADMIN").build();
+    manager.createUser(admin);
+    return manager;
   }
 }
