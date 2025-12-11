52bd6a1443ea5b7a3ac0b45f7cdfd7bd0262eea1
Noah Jutz <noah@noahjutz.com>
2025-12-11 13:57:47 +0100

    Auto-format

--- src/main/java/com/swdev/springbootproject/controller/SignupController.java
+++ src/main/java/com/swdev/springbootproject/controller/SignupController.java
@@ -1,7 +1,6 @@
 package com.swdev.springbootproject.controller;
 
 import com.swdev.springbootproject.entity.CbUser;
-import com.swdev.springbootproject.repository.CbUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
