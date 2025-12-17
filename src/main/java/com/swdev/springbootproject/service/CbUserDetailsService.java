50a0497bc20a55454e9fdba54608280024186144
Noah Jutz <noah@noahjutz.com>
2025-12-17 17:15:46 +0100

    Return unmodified CbUser from CbUserDetailsService

--- src/main/java/com/swdev/springbootproject/service/CbUserDetailsService.java
+++ src/main/java/com/swdev/springbootproject/service/CbUserDetailsService.java
@@ -1,10 +1,8 @@
 package com.swdev.springbootproject.service;
 
-import com.swdev.springbootproject.entity.CbUser;
 import com.swdev.springbootproject.repository.CbUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,11 +15,8 @@ class CbUserDetailsService implements UserDetailsService {
   private final CbUserRepository userRepository;
 
   @Override
+  @NonNull
   public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
-    return cbUserToUserDetails(userRepository.findByEmail(username));
-  }
-
-  private UserDetails cbUserToUserDetails(CbUser cbUser) {
-    return User.builder().username(cbUser.getEmail()).password(cbUser.getPassword()).build();
+    return userRepository.findByEmail(username);
   }
 }
