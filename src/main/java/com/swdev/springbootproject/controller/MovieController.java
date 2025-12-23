753aab1fa3125d47656dcc32a53ec124a8a623d4
Noah Jutz <noah@noahjutz.com>
2025-12-23 16:14:38 +0100

    Add movie details page

---
+++ src/main/java/com/swdev/springbootproject/controller/MovieController.java
@@ -0,0 +1,17 @@
+package com.swdev.springbootproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app/movie")
+public class MovieController {
+    @GetMapping("/{id}")
+    public String movie(@PathVariable int id, Model model) {
+        model.addAttribute(id);
+        return "movie_details";
+    }
+}
