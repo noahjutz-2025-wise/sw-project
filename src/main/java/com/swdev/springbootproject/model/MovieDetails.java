d641ef497525cbef60e14ca6a15361e35cc366e3
Noah Jutz <noah@noahjutz.com>
2025-12-31 18:31:56 +0100

    Add data class representing tmdb's /movie endpoint

---
+++ src/main/java/com/swdev/springbootproject/model/MovieDetails.java
@@ -0,0 +1,19 @@
+package com.swdev.springbootproject.model;
+
+import lombok.Data;
+
+@Data
+public class MovieDetails {
+    private String id;
+    private String title;
+    private String tagline;
+    private String overview;
+    private String backdropPath;
+    private String posterPath;
+    private String releaseDate;
+    private int revenue;
+    private int runtime;
+    private float popularity;
+    private float voteAverage;
+    private int voteCount;
+}
