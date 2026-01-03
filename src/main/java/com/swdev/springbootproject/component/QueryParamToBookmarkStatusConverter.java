1ca23f8076c30bb11d4965911864b0d2d67d8068
Noah Jutz <noah@noahjutz.com>
2026-01-03 17:58:56 +0100

    Apply google format

--- src/main/java/com/swdev/springbootproject/component/QueryParamToBookmarkStatusConverter.java
+++ src/main/java/com/swdev/springbootproject/component/QueryParamToBookmarkStatusConverter.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class QueryParamToBookmarkStatusConverter implements Converter<String, BookmarkStatus> {
 
-    @Override
-    public @Nullable BookmarkStatus convert(String source) {
-        return switch (source) {
-            case "watch-later" -> BookmarkStatus.WATCH_LATER;
-            case "in-progress" -> BookmarkStatus.IN_PROGRESS;
-            case "done" -> BookmarkStatus.DONE;
-            case "abandoned" -> BookmarkStatus.ABANDONED;
-            default -> throw new IllegalStateException("Unexpected value: " + source);
-        };
-    }
+  @Override
+  public @Nullable BookmarkStatus convert(String source) {
+    return switch (source) {
+      case "watch-later" -> BookmarkStatus.WATCH_LATER;
+      case "in-progress" -> BookmarkStatus.IN_PROGRESS;
+      case "done" -> BookmarkStatus.DONE;
+      case "abandoned" -> BookmarkStatus.ABANDONED;
+      default -> throw new IllegalStateException("Unexpected value: " + source);
+    };
+  }
 }
--- src/main/java/com/swdev/springbootproject/repository/MovieBookmarkRepository.java
+++ src/main/java/com/swdev/springbootproject/repository/MovieBookmarkRepository.java
@@ -3,10 +3,9 @@ package com.swdev.springbootproject.repository;
 import com.swdev.springbootproject.entity.CbUser;
 import com.swdev.springbootproject.entity.Movie;
 import com.swdev.springbootproject.entity.MovieBookmark;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MovieBookmarkRepository extends JpaRepository<MovieBookmark, Long> {
-    Optional<MovieBookmark> findByUserAndMovie(CbUser user, Movie movie);
+  Optional<MovieBookmark> findByUserAndMovie(CbUser user, Movie movie);
 }
