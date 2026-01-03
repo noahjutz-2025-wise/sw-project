ab820ecf158ad59f319882c39f108845ba94cef5
Noah Jutz <noah@noahjutz.com>
2026-01-03 16:42:13 +0100

    Add query param to BookmarkStatus converter

---
+++ src/main/java/com/swdev/springbootproject/component/QueryParamToBookmarkStatusConverter.java
@@ -0,0 +1,21 @@
+package com.swdev.springbootproject.component;
+
+import com.swdev.springbootproject.entity.BookmarkStatus;
+import org.jspecify.annotations.Nullable;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryParamToBookmarkStatusConverter implements Converter<String, BookmarkStatus> {
+
+    @Override
+    public @Nullable BookmarkStatus convert(String source) {
+        return switch (source) {
+            case "watch-later" -> BookmarkStatus.WATCH_LATER;
+            case "in-progress" -> BookmarkStatus.IN_PROGRESS;
+            case "done" -> BookmarkStatus.DONE;
+            case "abandoned" -> BookmarkStatus.ABANDONED;
+            default -> throw new IllegalStateException("Unexpected value: " + source);
+        };
+    }
+}
