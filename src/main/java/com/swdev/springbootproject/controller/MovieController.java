5779444a2d33d5e8f1d39d43de50b71224be5188
Noah Jutz <noah@noahjutz.com>
2026-01-03 17:56:38 +0100

    Dont pass bookmark status in GET /bookmark

--- src/main/java/com/swdev/springbootproject/controller/MovieController.java
+++ src/main/java/com/swdev/springbootproject/controller/MovieController.java
@@ -25,16 +25,12 @@ public class MovieController {
   private final QueryParamToBookmarkStatusConverter queryParamToBookmarkStatusConverter;
 
   @GetMapping("/{id}")
-  public String movie(@PathVariable Long id, Model model, Authentication authentication) {
+  public String movie(@PathVariable Long id, Model model) {
     final var movie = tmdbService.getMovieDetails(id);
-    final var bookmark =
-        movieBookmarkRepository.findByUserAndMovie(
-            (CbUser) authentication.getPrincipal(), new Movie(id));
     model.addAttribute(movie);
     model.addAttribute("poster", TMDBService.POSTER_BASE_URL + movie.getPosterPath());
     model.addAttribute("backdrop", TMDBService.BACKDROP_BASE_URL + movie.getBackdropPath());
     model.addAttribute("id", id);
-    model.addAttribute("bookmarkStatus", bookmark.map(MovieBookmark::getStatus).orElse(null));
     return "movie_details";
   }
 
