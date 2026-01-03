e092d29aec0831d4a92e751690b5cf8edde3c5b2
Noah Jutz <noah@noahjutz.com>
2026-01-03 16:06:27 +0100

    Add repositories for Movie and MovieBookmark

---
+++ src/main/java/com/swdev/springbootproject/repository/MovieRepository.java
@@ -0,0 +1,6 @@
+package com.swdev.springbootproject.repository;
+
+import com.swdev.springbootproject.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {}
