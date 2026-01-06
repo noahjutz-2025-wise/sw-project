9007f09d43c3535a9d8a5839934711e096b9da41
anastasiia-konstantynova <ana.konstantynova@gmail.com>
2026-01-06 17:23:52 +0100

    Add find methods

--- src/main/java/com/swdev/springbootproject/repository/FriendshipRepository.java
+++ src/main/java/com/swdev/springbootproject/repository/FriendshipRepository.java
@@ -2,6 +2,7 @@ package com.swdev.springbootproject.repository;
 
 import com.swdev.springbootproject.entity.Friendship;
 import java.util.List;
+import java.util.Optional;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,9 @@ public interface FriendshipRepository extends JpaRepository<@NonNull Friendship,
   List<@NonNull Friendship> findBySender_IdOrReceiver_Id(
       @NonNull Long userId1, @NonNull Long userId2);
 
-  boolean existsByCbUser1_IdAndCbUser2_Id(@NonNull Long userId1, @NonNull Long userId2);
+  Optional<Friendship> findBySender_IdAndReceiver_Id(Long senderId, Long receiverId);
+
+  Optional<Friendship> findFriendshipById(Long id);
+
+  boolean existsBySender_IdAndReceiver_Id(@NonNull Long userId1, @NonNull Long userId2);
 }
