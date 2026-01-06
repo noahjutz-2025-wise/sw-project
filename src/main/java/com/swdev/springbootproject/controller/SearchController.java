a966f0503cf25e7f19c13110298e29be59d7a177
Noah Jutz <noah@noahjutz.com>
2026-01-06 17:33:59 +0100

    Apply google format

--- src/main/java/com/swdev/springbootproject/controller/SearchController.java
+++ src/main/java/com/swdev/springbootproject/controller/SearchController.java
@@ -1,14 +1,12 @@
 package com.swdev.springbootproject.controller;
 
 import com.swdev.springbootproject.service.TMDBService;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-import java.util.Random;
 
 @RequiredArgsConstructor
 @Controller
