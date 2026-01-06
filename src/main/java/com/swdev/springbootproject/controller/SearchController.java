40b72e5954888eb924acff9e055edcf6693a2891
Noah Jutz <noah@noahjutz.com>
2026-01-06 17:26:48 +0100

    Add results container to search (mock data for now)

--- src/main/java/com/swdev/springbootproject/controller/SearchController.java
+++ src/main/java/com/swdev/springbootproject/controller/SearchController.java
@@ -3,8 +3,12 @@ package com.swdev.springbootproject.controller;
 import com.swdev.springbootproject.service.TMDBService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,5 +19,12 @@ class SearchController {
   @GetMapping("")
   public String search() {
     return "search";
+  }
+
+  @GetMapping("results")
+  public String searchResults(Model model) {
+    final var movies = tmdbService.getPopularMovies(1).subList(0, new Random().nextInt(10));
+    model.addAttribute("movies", movies);
+    return "fragments/movie_card_grid :: movieCardGrid(movies=${movies})";
   }
 }
