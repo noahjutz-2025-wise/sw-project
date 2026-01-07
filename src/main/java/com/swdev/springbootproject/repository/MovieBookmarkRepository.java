62ab25c4fdeafab0edcf793906f4c4ff88d0e356
Noah Jutz <noah@noahjutz.com>
2026-01-07 20:26:03 +0100

    Show bookmarked movies in watchlist
    AI generated using Junie with the Prompt:
    
    > add the /app/watchlist/results endpoint to WatchlistController, using the MovieBookmarkRepository to get the IDs of saved movies (in CbMovie). Then stream map the MovieBookmarks or CbMovies to TmdbMovies by calling TMDBService.getMovieDetails for each. Then turn the TmdbMovies into MovieDtos by using the converter in the component package. Finally, pass the MovieDtos to the movie_card_grid and serve that fragment as a return value.
    >
    > This workflow is similar to the SearchController's results endpoint.

--- src/main/java/com/swdev/springbootproject/repository/MovieBookmarkRepository.java
+++ src/main/java/com/swdev/springbootproject/repository/MovieBookmarkRepository.java
@@ -1,11 +1,15 @@
 package com.swdev.springbootproject.repository;
 
+import com.swdev.springbootproject.entity.BookmarkStatus;
 import com.swdev.springbootproject.entity.CbMovie;
 import com.swdev.springbootproject.entity.CbUser;
 import com.swdev.springbootproject.entity.MovieBookmark;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieBookmarkRepository extends JpaRepository<MovieBookmark, Long> {
   Optional<MovieBookmark> findByUserAndMovie(CbUser user, CbMovie cbMovie);
+
+  List<MovieBookmark> findByUserAndStatus(CbUser user, BookmarkStatus status);
 }
