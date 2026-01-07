802051f02dc9169239fbca6e7376ccffd56de077
Noah Jutz <noah@noahjutz.com>
2026-01-07 15:07:43 +0100

    Add all query params to /app/movie/results

--- src/main/java/com/swdev/springbootproject/controller/SearchController.java
+++ src/main/java/com/swdev/springbootproject/controller/SearchController.java
@@ -1,12 +1,16 @@
 package com.swdev.springbootproject.controller;
 
+import com.swdev.springbootproject.model.tmdb.Movie;
 import com.swdev.springbootproject.service.TMDBService;
+
+import java.util.List;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,7 +24,20 @@ class SearchController {
   }
 
   @GetMapping("results")
-  public String searchResults(Model model) {
+  public String searchResults(
+          @RequestParam(defaultValue = "") String search,
+          @RequestParam(defaultValue = "movies") String searchType,
+          @RequestParam(defaultValue = "false") boolean isIncludeSeen,
+          @RequestParam(required = false) String genre,
+          @RequestParam(required = false) Integer durationMin,
+          @RequestParam(required = false) Integer durationMax,
+          @RequestParam(required = false) Integer fsk,
+          @RequestParam(required = false) Integer ratingMin,
+          @RequestParam(required = false) Integer ratingMax,
+          @RequestParam(required = false) Integer yearMin,
+          @RequestParam(required = false) Integer yearMax,
+          Model model
+  ) {
     final var movies = tmdbService.getPopularMovies(1).subList(0, new Random().nextInt(10));
     model.addAttribute("movies", movies);
     return "fragments/movie_card_grid :: movieCardGrid(movies=${movies})";
