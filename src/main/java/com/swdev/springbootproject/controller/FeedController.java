cc52aec7aa2372d0315bd13c89bca8c82bf9d4f7
Noah Jutz <noah@noahjutz.com>
2026-01-14 19:33:08 +0100

    Add empty Feed page

---
+++ src/main/java/com/swdev/springbootproject/controller/FeedController.java
@@ -0,0 +1,15 @@
+package com.swdev.springbootproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FeedController {
+
+    @GetMapping("/feed")
+    public String showFeed(Model model) {
+        model.addAttribute("pageTitle", "Feed");
+        return "feed";
+    }
+}
