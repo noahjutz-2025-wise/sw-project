d3570c4d58506957c5abbdea521c94545a0b3fd1
Noah Jutz <noah@noahjutz.com>
2026-01-17 17:55:54 +0100

    Implement post button (endpoint is a stub)

--- src/main/java/com/swdev/springbootproject/controller/FeedController.java
+++ src/main/java/com/swdev/springbootproject/controller/FeedController.java
@@ -1,16 +1,19 @@
 package com.swdev.springbootproject.controller;
 
 import com.swdev.springbootproject.component.*;
+import com.swdev.springbootproject.model.dto.MediaDto;
 import com.swdev.springbootproject.service.TMDBService;
+
+import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/app/feed")
 @RequiredArgsConstructor
 public class FeedController {
   private @NonNull StringToJsonNodesConverter stringToJsonNodes;
@@ -19,26 +22,38 @@ public class FeedController {
   private @NonNull TmdbTvToMediaDtoConverter tvToMedia;
   private @NonNull TmdbMovieToMediaDtoConverter movieToMedia;
 
-  @GetMapping("/app/feed")
+  @GetMapping()
   public String showFeed(Model model) {
     model.addAttribute("pageTitle", "Feed");
     return "feed";
   }
 
-  @GetMapping("/app/feed/mediaCards")
+  @GetMapping("/mediaCards")
   public String showFeedMediaCards(@RequestParam("media_json") String medias, Model model) {
-    final var mediaDtos =
-        Objects.requireNonNull(stringToJsonNodes.convert(medias)).stream()
-            .map(jsonNodeToMedia::convert)
-            .filter(Objects::nonNull)
-            .map(
-                it ->
-                    switch (it.getType()) {
-                      case MOVIE -> movieToMedia.convert(tmdbService.getMovieDetails(it.getId()));
-                      case TV -> tvToMedia.convert(tmdbService.getTvDetails(it.getId()));
-                    })
-            .toList();
+    final var mediaDtos = stringToMediaDtos(medias);
     model.addAttribute("movies", mediaDtos);
     return "fragments/movie_card_row::movie_card_row";
+  }
+
+  @PostMapping()
+  @ResponseBody
+  public String post(
+      @RequestParam("media_json") String medias,
+      @RequestParam("post-text") String postText,
+      Model model) {
+    return stringToMediaDtos(medias).toString() + postText;
+  }
+
+  private List<MediaDto> stringToMediaDtos(String medias) {
+    return Objects.requireNonNull(stringToJsonNodes.convert(medias)).stream()
+        .map(jsonNodeToMedia::convert)
+        .filter(Objects::nonNull)
+        .map(
+            it ->
+                switch (it.getType()) {
+                  case MOVIE -> movieToMedia.convert(tmdbService.getMovieDetails(it.getId()));
+                  case TV -> tvToMedia.convert(tmdbService.getTvDetails(it.getId()));
+                })
+        .toList();
   }
 }
