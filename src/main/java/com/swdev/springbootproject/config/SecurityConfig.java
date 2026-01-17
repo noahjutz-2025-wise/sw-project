c0cebde7abb80bfd85d9b6ea20546f3725fdad53
QuocViet-Pham <72968949+QuocViet-Pham@users.noreply.github.com>
2026-01-17 02:57:33 +0100

    Update SecurityConfig, fix path after login to mood page

--- src/main/java/com/swdev/springbootproject/config/SecurityConfig.java
+++ src/main/java/com/swdev/springbootproject/config/SecurityConfig.java
@@ -39,7 +39,7 @@ class SecurityConfig {
                     .permitAll()
                     .anyRequest()
                     .authenticated())
-        .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home").permitAll())
+        .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/mood").permitAll())
         .csrf(csrf -> csrf.ignoringRequestMatchers("/**"))
         .build();
   }
