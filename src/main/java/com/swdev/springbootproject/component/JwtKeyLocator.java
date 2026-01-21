86389656689c21410262e661ff487447bfa0309f
Noah Jutz <noah@noahjutz.com>
2026-01-21 14:28:27 +0100

    Add JwtRequestFilter

---
+++ src/main/java/com/swdev/springbootproject/component/JwtKeyLocator.java
@@ -0,0 +1,20 @@
+package com.swdev.springbootproject.component;
+
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Locator;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtKeyLocator implements Locator<Key> {
+  @Value("${jwt.secret}")
+  private String key;
+
+  @Override
+  public Key locate(Header header) {
+    return Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8));
+  }
+}
