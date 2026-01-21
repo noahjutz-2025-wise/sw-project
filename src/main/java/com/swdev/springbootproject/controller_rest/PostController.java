71eada97470fa75ec7c857c14c008717af74c957
Noah Jutz <noah@noahjutz.com>
2026-01-21 12:25:16 +0100

    Implement various http methods for PostController

--- src/main/java/com/swdev/springbootproject/controller_rest/PostController.java
+++ src/main/java/com/swdev/springbootproject/controller_rest/PostController.java
@@ -1,29 +1,23 @@
 package com.swdev.springbootproject.controller_rest;
 
+import com.swdev.springbootproject.component.PostDtoToPostConverter;
 import com.swdev.springbootproject.component.PostToPostDtoConverter;
-import com.swdev.springbootproject.entity.Post;
 import com.swdev.springbootproject.model.dto.PostDto;
 import com.swdev.springbootproject.repository.PostRepository;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/posts")
+@RequiredArgsConstructor
 @ResponseBody
 public class PostController {
   private final PostRepository postRepository;
   private final PostToPostDtoConverter postToPostDtoConverter;
-
-  public PostController(
-      PostRepository postRepository, PostToPostDtoConverter postToPostDtoConverter) {
-    this.postRepository = postRepository;
-    this.postToPostDtoConverter = postToPostDtoConverter;
-  }
+  private final PostDtoToPostConverter postDtoToPostConverter;
 
   @GetMapping
   public List<PostDto> posts(@RequestParam(defaultValue = "0") int page) {
@@ -32,5 +26,29 @@ public class PostController {
         .map(postToPostDtoConverter::convert)
         .stream()
         .toList();
+  }
+
+  @GetMapping("/{id}")
+  public PostDto post(@PathVariable Long id) {
+    return postToPostDtoConverter.convert(postRepository.findById(id).orElseThrow());
+  }
+
+  @PostMapping
+  public PostDto createPost(@RequestBody PostDto postDto) {
+    assert postDto.getId() == null;
+    final var post = postDtoToPostConverter.convert(postDto);
+    return postToPostDtoConverter.convert(postRepository.save(post));
+  }
+
+  @DeleteMapping("/{id}")
+  public void deletePost(@PathVariable Long id) {
+    postRepository.deleteById(id);
+  }
+
+  @PutMapping
+  public PostDto updatePost(@RequestBody PostDto postDto) {
+    assert postDto.getId() != null;
+    final var post = postDtoToPostConverter.convert(postDto);
+    return postToPostDtoConverter.convert(postRepository.save(post));
   }
 }
